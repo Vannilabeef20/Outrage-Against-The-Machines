5703fdef6556edc849aae56ee2f337b3fadff400
Vannilabeef20 <112733952+Vannilabeef20@users.noreply.github.com>
2024-08-21 00:11:33 -0300

    20/08 Bugfix
    - Volume agora inicializa direito

--- Assets/Game/Code/Static Code/AudioManager.cs
+++ Assets/Game/Code/Static Code/AudioManager.cs
@@ -3,6 +3,7 @@ using UnityEngine.Audio;
 using NaughtyAttributes;
 using UnityEngine.SceneManagement;
 using FMODUnity;
+using FMOD.Studio;
 
 namespace Game
 {
@@ -18,6 +19,10 @@ namespace Game
         [SerializeField, ReadOnly] int uiClickNegativeSoundIndex;
         [SerializeField, ReadOnly] int uiSelectSoundIndex;
 
+        [Header("VOLUME INIT"), HorizontalLine(2f, EColor.Orange)]
+        [SerializeField] AudioVolumeCurveSO volumeCurve;
+        [SerializeField, ReadOnly] string[] busNames = new string[] { "Master", "Music", "SFX" };
+
         private void Awake()
         {
             if (instance == null)
@@ -28,6 +33,22 @@ namespace Game
             else
             {
                 Destroy(gameObject);
+            }
+
+            Bus tempBus;
+            foreach (var busName in busNames)
+            {
+                if (busName == busNames[0])
+                {
+                    tempBus = RuntimeManager.GetBus("bus:/");
+                }
+                else
+                {
+                    tempBus = RuntimeManager.GetBus($"bus:/Master/{busName}");
+                }
+                float db = volumeCurve.Curve.Evaluate(PlayerPrefs.GetFloat(busName, 0.5f));
+                float volume = Mathf.Pow(10f, db / 20f);
+                tempBus.setVolume(volume);
             }
         }
      
